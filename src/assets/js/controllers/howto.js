8b93a90ab70134174e7dbac9fda6252fb37cc5e8
Pankamol Srikaew <aim.ske09@gmail.com>
2017-10-19 01:24:00 +0700

    how to templates

--- src/assets/js/controllers/howto.js
+++ src/assets/js/controllers/howto.js
@@ -1,6 +1,29 @@
 angular.module('app')
 .controller('HowtoCtrl',['$scope', '$http','$window',
 	function($scope, $http, $window){
+
+		$scope.steps = [
+		{
+			"no": "1",
+			"description": "step 1",
+			"img": ""
+		},
+		{
+			"no": "2",
+			"description": "step 2",
+			"img": ""
+		},
+		{
+			"no": "3",
+			"description": "step 3",
+			"img": ""
+		},
+		{
+			"no": "4",
+			"description": "step 4",
+			"img": ""
+		}
+		]
 		
 	}
 ]);
