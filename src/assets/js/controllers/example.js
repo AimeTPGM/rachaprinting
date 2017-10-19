853dbefc4509375a92bd484905f8fd80c4dea7e3
Pankamol Srikaew <aim.ske09@gmail.com>
2017-10-19 14:27:18 +0700

    init example

---
+++ src/assets/js/controllers/example.js
@@ -0,0 +1,52 @@
+angular.module('app')
+.controller('ExampleCtrl',['$scope', '$http','$window',
+	function($scope, $http, $window){
+		$scope.examples = [
+			{
+				"name": "",
+				"img": "",
+				"price": "100" 
+			},
+			{
+				"name": "",
+				"img": "",
+				"price": "100" 
+			},
+			{
+				"name": "",
+				"img": "",
+				"price": "100" 
+			},
+			{
+				"name": "",
+				"img": "",
+				"price": "100" 
+			},
+			{
+				"name": "",
+				"img": "",
+				"price": "100" 
+			},
+			{
+				"name": "",
+				"img": "",
+				"price": "100" 
+			},
+			{
+				"name": "",
+				"img": "",
+				"price": "100" 
+			},
+			{
+				"name": "",
+				"img": "",
+				"price": "100" 
+			},
+			{
+				"name": "",
+				"img": "",
+				"price": "100" 
+			}
+		]
+	}
+]);
