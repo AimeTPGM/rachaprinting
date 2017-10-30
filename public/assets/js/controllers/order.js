040b7f30a7a2c6f0a11966003b4cf71026866438
Pankamol Srikaew <aim.ske09@gmail.com>
2017-10-30 16:58:36 +0700

    add order detail

--- public/assets/js/controllers/order.js
+++ public/assets/js/controllers/order.js
@@ -4,7 +4,22 @@ angular.module('app')
 		$scope.templates = {
 			'navbar' : '/views/navbar.html',
 			'product' : '/views/product-slide.html',
-			'orderForm' : '/views/order-form.html'
+			'orderForm' : '/views/order-form.html',
+			'orderDetailFixed' : '/views/order-detail-fixed.html',
+			'orderDetail' : '/views/order-detail.html',
+			'footer' : '/views/footer.html',
+			'credit' : '/views/credit.html'
 		}
+		
+		angular.element($window).bind("scroll", function() {
+			var orderDetailPostion = angular.element(document.querySelector('#orderDetail'))[0].offsetHeight
+			console.log(orderDetailPostion)
+			if (this.pageYOffset >= orderDetailPostion) {
+				angular.element(document.querySelector('#orderDetailFixed')).css('display', 'none')
+			}
+			else if (this.pageYOffset < orderDetailPostion) {
+				angular.element(document.querySelector('#orderDetailFixed')).css('display', 'block')
+			}
+		})
 	}
 ]);
