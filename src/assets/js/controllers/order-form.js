175d757e551bf5dfb6b4de1eaa6f200cbefa60a2
Pankamol Srikaew <aim.ske09@gmail.com>
2017-11-17 01:09:31 +0700

    mock order page

--- src/assets/js/controllers/order-form.js
+++ src/assets/js/controllers/order-form.js
@@ -9,12 +9,36 @@ angular.module('app')
 			console.log($scope.selectedValue)
 		}
 
-		$scope.templateList = [{id:1, name: 'Please Select'}, {id:2, name: 'Template1'}, {id:3, name: 'Template2'}]
-
+		$scope.templateList = [{id:1, name: 'A4', description: 'print as A4'}, {id:2, name: 'A3', description: 'print as A3'}, {id:3, name: 'A5', description: 'print as A5'}]
+		$scope.prints = [{id:1, name: 'One-side', description: 'print one side'},{id:2, name: 'Two-side', description: 'print two side'}]
+		$scope.printColors = [{id:1, name: 'Black & White', description: 'print black & white'},{id:2, name: 'Color', description: 'print color'}]
+		$scope.paperTypes = [{id:1, name: '200 gram', description: '200 gram'},{id:2, name: '180 gram', description: '180 gram'},{id:3, name: '150 gram', description: '150 gram'},{id:4, name: '130 gram', description: '130 gram'},{id:5, name: '100 gram', description: '100 gram'},{id:6, name: '80 gram', description: '80 gram'}]
+		$scope.refinements = [{id:1, name: 'None', description: 'None'}, {id:2, name: 'UV', description: 'UV'}]
+		
 		$scope.selectedValue = {}
-		$scope.setValue = function(list){
+		$scope.setMainChoice = function(){
 
-			
 		}
+
+		$scope.setFormat = function(){
+
+		}
+
+		$scope.setPrint = function(){
+
+		}
+
+		$scope.setPrintColor = function(){
+
+		}
+
+		$scope.setPaperType = function(){
+
+		}
+
+		$scope.setRefinement = function(){
+
+		}
+
 	}
 ]);
