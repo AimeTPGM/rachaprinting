7ac44d2a50d73d6760d415dfe79484b73c3fc77a
Pankamol Srikaew <aim.ske09@gmail.com>
2017-11-18 03:00:53 +0700

    semi completed order detail form

--- public/assets/js/controllers/order-detail.js
+++ public/assets/js/controllers/order-detail.js
@@ -1,11 +1,19 @@
 angular.module('app')
-.controller('OrderDetailCtrl',['$scope', '$http','$window', 'OrderDetailService',
-	function($scope, $http, $window, OrderDetailService){
+.controller('OrderDetailCtrl',['$scope', '$http','$window', '$timeout', 'OrderDetailService',
+	function($scope, $http, $window, $timeout, OrderDetailService){
 		$scope.orderDetail = OrderDetailService.orderDetail
 		console.log(OrderDetailService.orderDetail)
-
+		$scope.countdown = 5
+		$scope.customer = {}
 		$scope.placeOrder = function(){
-			
+			$scope.completedOrder = true
+			OrderDetailService.orderDetail.customer = $scope.customer
+			console.log(OrderDetailService.orderDetail)
+			$timeout(function() {
+				$window.location.href = "/"
+			}, 5000)
+
+
 		}
 	}
 ]);
