1886ce088298dbd953f8c31512fa85df41302dbc
Pankamol Srikaew <aim.ske09@gmail.com>
2017-11-17 17:14:51 +0700

    fix css and add navbar-home

---
+++ public/assets/js/controllers/navbar-home.js
@@ -0,0 +1,47 @@
+angular.module('app')
+.controller('NavbarHomeCtrl',['$scope', '$http','$window', '$document',
+	function($scope, $http, $window, $document){
+		$scope.menus = [
+			{
+				'name': 'บริการของเรา',
+				'section': 'service'
+			},
+			{
+				'name': 'วิธีสั่งซื้อ',
+				'section': 'howto'
+			},
+			{
+				'name': 'รีวิว',
+				'section': 'review'
+			},
+			{
+				'name': 'ผลงานที่ผ่านมา',
+				'section': 'example'
+			}
+		]
+		var showMobile = false
+		$scope.navMobile = function(){
+			if(showMobile == false){
+				showMobile = true
+				$scope.show = function(){
+					return true;
+				}
+			}
+			else {
+				showMobile = false
+				$scope.show = function(){
+					return false;
+				}
+			}
+			
+		}
+
+		$scope.scrollTo = function(id){
+			$document.scrollToElementAnimated(angular.element(document.getElementById(id)), 72)
+		}
+
+		
+		
+
+	}
+]);
