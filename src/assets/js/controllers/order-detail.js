c75efaa9a2831d0bdef0db16fa2ffa9ac5c201b9
Pankamol Srikaew <aim.ske09@gmail.com>
2017-11-18 02:15:35 +0700

    add order detai form

--- src/assets/js/controllers/order-detail.js
+++ src/assets/js/controllers/order-detail.js
@@ -1,5 +1,11 @@
 angular.module('app')
 .controller('OrderDetailCtrl',['$scope', '$http','$window', 'OrderDetailService',
 	function($scope, $http, $window, OrderDetailService){
+		$scope.orderDetail = OrderDetailService.orderDetail
+		console.log(OrderDetailService.orderDetail)
+
+		$scope.placeOrder = function(){
+			
+		}
 	}
 ]);
